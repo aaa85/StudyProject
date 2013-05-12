498c73df1dddd2b7ac67dbbaa7586fa290c40309
aaa85 <mmrmmlrr@gmail.com>
2013-05-12 22:56:42 +0300

    initial commit

---
+++ someProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  someProject
+//
+//  Created by Aleksey on 12.05.13.
+//  Copyright (c) 2013 Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ someProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  someProject
+//
+//  Created by Aleksey on 12.05.13.
+//  Copyright (c) 2013 Aleksey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
